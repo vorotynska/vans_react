72b66cdf83451b05da914ef28b5872a081f280c6
Mykola Vorotynskyy <vorotynskyy@gmail.com>
2023-05-23 00:19:06 +0200

    commit

--- vanlife/src/App.js
+++ vanlife/src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home"
 import About from './pages/About';
 import Vans from './pages/Vans';
 import VanDetail from './pages/VanDetail';
+import Layout from './components/Layout';
 
 import "./server"
 
@@ -12,18 +13,14 @@ function App() {
   return (
  
     <BrowserRouter>
-    <header>
-      <Link className='site-logo' to="/">#VANLIFE</Link>
-      <nav>
-        <Link to="/about">About</Link>
-        <Link to="vans">Vans</Link>
-      </nav>
-    </header>
+ 
        <Routes>
-        <Route path='/' element={ <Home />} />
+        <Route element={<Layout />}>
+           <Route path='/' element={ <Home />} />
+           <Route path='/vans' element={<Vans />} />
+            <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
         <Route path='/about' element={ <About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
        </Routes>
     </BrowserRouter>
    
